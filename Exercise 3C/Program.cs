8e0bae3b574d5b274dc69abfb83e7117e5abdd7a
grace <grace@CHIAKI-LIV3S-HE>
2022-03-15 17:45:42 +1300

    Add project files.

---
+++ Exercise 3C/Program.cs
@@ -0,0 +1,16 @@
+﻿
+{
+    int a, b, c, perimeter, s, area;
+
+    Console.WriteLine("\nPlease Enter three sides of triangle\n");
+   a = Convert.ToInt32(Console.ReadLine());
+    b = Convert.ToInt32(Console.ReadLine());
+    c = Convert.ToInt32(Console.ReadLine());
+    perimeter = a + b + c;
+    s = Convert.ToInt32(a + b + c) / 2;
+    area = (s * (s - a) * (s - b) * (s - c));
+
+    Console.WriteLine($" Area of the triangle: {area}");
+
+    return 0;
+}
